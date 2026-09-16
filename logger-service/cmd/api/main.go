d7303903df04022c4ccb029d7dac5cd96f0c3e76
Asanbek09 <samudinua@gmail.com>
2026-09-16 12:06:19 +0600

    Начало работы с сервисом logger-service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	grpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	// create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+	clientOptions.SetConnectTimeout(10 * time.Second)
+
+	// connect
+	c, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Println("error connecting:", err)
+		return nil, err
+	}
+
+	return c, nil
+}
